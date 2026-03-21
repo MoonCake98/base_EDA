85fbff2287c5300218f9fe16f70d3ae3e6b76228
Mooncake98_laptop <t.t.vo@st.hanze.nl>
2026-03-21 06:19:03 +0100

    working panel server instance with alert testing

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ print(sys.executable)
 # configures panel en preloads the specified extensions and widget support
 pn.extension("plotly", "vega", "tabulator")
 
-# create displayable panel component
-test = pn.panel("Hello, Panel!")
+# test alert types
+text = (
+    "This is a **{alert_type}** alert with [an example link]"
+    "(https://panel.holoviz.org/). Give it a click if you like."
+)
+
+test_alert = pn.Column(*[
+    pn.pane.Alert(text.format(alert_type=at), alert_type=at)
+    for at in pn.pane.Alert.param.alert_type.objects],
+    sizing_mode="stretch_width"
+).servable()
+
+# label displayable panel component
+test = test_alert
 
 if __name__ == "__main__":
     test.servable() # displays component in server app
