457ca08137ba725163aafa41cf66921ed960fd56
Mooncake98_laptop <t.t.vo@st.hanze.nl>
2026-04-17 14:33:15 +0200

    converted the dataframe component into the tabulator widget for more control and future proofing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print(sys.executable)
 
 # cli to start panel server: `panel serve main.py`
 
-# cli for dev mode panel serve app.py --dev --show --autoreload 
+# cli for dev mode | panel serve app.py --dev --show --autoreload 
 # reloads app on file changes and opens in browser automatically
 
 # configures panel en preloads the specified extensions and widget support
@@ -38,14 +38,19 @@ test_file_input = pn.widgets.FileInput(accept=".csv", multiple=False)
 # pn.bind(test_file_input, print(test_file_input.value))
 
 
-table = pn.pane.DataFrame()
+table = pn.Column(
+    pn.pane.Markdown(
+        "### No data loaded yet. Please select a CSV file and click 'Load CSV' to display the data."
+        )
+)
 
 def load_csv(event):
     if test_file_input.value is not None:
         df = pd.read_csv(io.BytesIO(test_file_input.value))
-        table.object = df
+        global table
+        table.clear()
+        table.append(pn.widgets.Tabulator(df))
         print("data loading complete")
-
 submit_test.on_click(load_csv)
 
 
